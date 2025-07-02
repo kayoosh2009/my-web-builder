4d1a30ce6e0d1df89b4ec7f0dc5fbb5007f80d41
Kai Miyamoto <kayoosh2009@gmail.com>
2025-07-02 15:09:42 +0300

    Create main.js

---
+++ script/main.js
@@ -0,0 +1,7 @@
+import { setupSidebar } from '../components/sidebar.js';
+import { setupCanvas } from '../components/canvas.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+  setupSidebar();
+  setupCanvas();
+});
