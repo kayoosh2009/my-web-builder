f9c76a2c5a337a030f5c902b7858a4cfaa5de138
Kai Miyamoto <kayoosh2009@gmail.com>
2025-07-03 14:09:43 +0300

    Update canvas.js

--- components/canvas.js
+++ components/canvas.js
@@ -1,4 +1,31 @@
+let activeElement = null;
+
 export function setupCanvas() {
   const canvas = document.getElementById("canvas");
-  // Пока ничего, но потом сюда добавим drag-n-drop, настройки и т.д.
+
+  canvas.addEventListener("click", (e) => {
+    if (e.target.closest("#canvas > div")) {
+      setActiveElement(e.target.closest("#canvas > div"));
+    } else {
+      clearActiveElement();
+    }
+  });
 }
+
+function setActiveElement(el) {
+  clearActiveElement();
+  activeElement = el;
+  el.classList.add("active");
+
+  const event = new CustomEvent("elementSelected", {
+    detail: el,
+  });
+  window.dispatchEvent(event);
+}
+
+function clearActiveElement() {
+  if (activeElement) activeElement.classList.remove("active");
+  activeElement = null;
+  window.dispatchEvent(new Event("elementUnselected"));
+}
+
