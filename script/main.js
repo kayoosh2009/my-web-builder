147645d8f6d74c5ad693da6d09a12c344eec8e47
Kai Miyamoto <kayoosh2009@gmail.com>
2025-07-03 14:45:36 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,10 +1,11 @@
 import { setupSidebar } from '../components/sidebar.js';
 import { setupCanvas } from '../components/canvas.js';
 import { setupSettingsPanel } from '../components/settingsPanel.js';
+import { setupToolbar } from '../components/toolbar.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   setupSidebar();
   setupCanvas();
   setupSettingsPanel();
+  setupToolbar();
 });
-
